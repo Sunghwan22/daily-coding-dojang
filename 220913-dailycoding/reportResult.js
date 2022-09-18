0cb1263178a1f2b55f0bc63a6440c8b09c4beae0
sunghwan22 <tidls3144@naver.com>
2022-09-18 14:41:01 +0900

    프로그래머스 신고 결과 받기 자바스크립트

--- 220913-dailycoding/reportResult.js
+++ 220913-dailycoding/reportResult.js
@@ -1,10 +1,56 @@
-/* eslint-disable import/prefer-default-export */
-/* 먼저 report에 있는 중복을 제거해야할 것 같다 자바스크립트도 set을 사용하면 될듯? */
-/*   */
+export function removeDuplicateItem(array) {
+  const set = new Set(array);
+  const newArray = [...set];
+  return newArray;
+}
 
-export function solution(idList, report, k) {
-  const reportWithoutOverlap = [...new Set(report)];
-  const reported = report.map((item) => item.split(' ')[1]);
-  const FinalReport = [];
-  const count = idList.map((id) => id.fill(0));
+export function convertToReportedArray(array) {
+  const result = array.map((item) => item.split(' '));
+  return result;
+}
+
+export function makeReportingArray(idList, uniqueArray) {
+  const reportingArray = Array(idList.length).fill(0);
+
+  const result = reportingArray.map((_, index) => (
+    { user: idList[index], reporting: [] }));
+
+  return result;
+}
+
+export function countReportedNumber(id_list, reportedArray) {
+  const newReportedArray = [];
+
+  for (let i = 0; i < id_list.length; i += 1) {
+    let number = 0;
+
+    for (let j = 0; j < reportedArray.length; j += 1) {
+      if (id_list[i] === [reportedArray[j]][1]) {
+        number += 1;
+      }
+    }
+
+    newReportedArray.push([id_list[i], number]);
+  }
+
+  return newReportedArray;
+}
+
+function solution(id_list, report, k) {
+  const answer = [];
+
+  // 1. 리포트 배열에서 중복값 제거
+  const uniqueArray = removeDuplicateItem(report);
+
+  // 2-1. 리포터 배열의 각 원소를 split으로 쪼개서 [신고자, 신고당한이] 배열로 만듦
+  const reportedArray = convertToReportedArray(uniqueArray);
+
+  // 2-2. id_list와 크기, 원소 순서가 같은 배열을 만들고, 그 배열의 원소가 {신고자, [신고한사람 명단]}
+  // 이런 형태를 가질 수 있도록.해보자.
+
+  const reportingArray = makeReportingArray(id_list, uniqueArray);
+
+  countReportedNumber(id_list, reportedArray);
+
+  return answer;
 }
