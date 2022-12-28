02fa47e7ecde9bb9baa6efd9e0330a30885a405b
sunghwan22 <tidls3144@naver.com>
2022-12-28 11:21:19 +0900

    프로그래머스 귤 고르기

---
+++ 221228-dailycoding/app/src/test/java/SolutionTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void example() {
+        Solution solution = new Solution();
+        //
+        assertEquals("noraj", solution.solution("jaron"));
+        assertEquals("noraj", solution.solution("jaron"));
+    }
+}
