dd6084578527fb1f78ab87e73f6925a7add34b13
sunghwan22 <tidls3144@naver.com>
2023-01-03 09:54:28 +0900

    프로그래머스 마법의 엘리베이터

---
+++ 230102-dailycoding/solution.js
@@ -0,0 +1,10 @@
+export default function solution(storey) {
+  if (storey < 5) {
+    return storey;
+  }
+
+  const rest = storey % 10;
+  const value = (storey - rest) / 10;
+
+  return Math.min(r + solution(value), 10 - rest + solution(value + 1));
+}
---
+++ 230102-dailycoding/solution.test.js
@@ -0,0 +1,6 @@
+import { solution } from './solution';
+
+test('example', () => {
+  expect(solution(2554).toBe(16));
+  expect(solution(16).toBe(6));
+});
