5243a5f8136500fe310b16a16e56081dd60a380a
sunghwan22 <tidls3144@naver.com>
2023-03-19 17:29:12 +0900

    프로그래머스 내 마음대로 문자열 정렬하기

---
+++ 230319-dailycoding/app/src/test/java/SolutionTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void example() {
+        Solution solution = new Solution();
+
+        assertArrayEquals(new String[]{"car", "bed", "sun"}, solution.solution(
+            new String[]{"sun", "bed", "car"}, 1
+        ));
+
+        assertArrayEquals(new String[]{"abcd", "abce", "cdx"}, solution.solution(
+            new String[]{"abce", "abcd", "cdx"}, 1
+        ));
+    }
+}
