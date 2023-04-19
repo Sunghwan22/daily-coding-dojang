7f6c47d8dc70cae69dd3c07946c78c024d448be3
sunghwan22 <tidls3144@naver.com>
2023-04-19 16:36:36 +0900

    Do it 자바 평균 구하기

--- 230419-dailycoding/app/src/main/java/Solution.java
+++ 230419-dailycoding/app/src/main/java/Solution.java
@@ -3,29 +3,24 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String sNum = scanner.nextLine();
+        int subjectNumber = scanner.nextInt();
+        int[] scores =  new int[subjectNumber];
 
-        char[] charArray = sNum.toCharArray();
-
-        int sum = 0;
-
-        for(int i = 0; i< charArray.length; i += 1) {
-            sum += charArray[i] - '0';
+        for(int i = 0; i < subjectNumber; i +=1) {
+            scores[i] = scanner.nextInt();
         }
 
-        System.out.print(sum);
-    }
+        long sum = 0;
+        long maxScore = 0;
 
-    public int solution(int n, String sNum) {
-        char[] charArray = sNum.toCharArray();
+        for(int i = 0; i < subjectNumber; i +=1) {
+            if(scores[i] > maxScore) {
+                maxScore = scores[i];
+            }
 
-        int sum = 0;
-
-        for(int i =0; i < charArray.length; i +=1) {
-            sum += charArray[i] - '0';  // 아스키 코드 이키 떄문에 인트로 바꿔주지 않으면 값이 다르게 나온다.
+            sum += scores[i];
         }
 
-        return sum;
+        System.out.print((sum * 100.0) / maxScore / subjectNumber);
     }
 }
