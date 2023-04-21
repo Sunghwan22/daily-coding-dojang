eeb14a2511ef727a98f895d7b483c954e3442279
sunghwan22 <tidls3144@naver.com>
2023-04-21 16:38:58 +0900

    연속된 자연수의 합 구하기 (투 포인터 전략)

--- 230419-dailycoding/app/src/main/java/Solution.java
+++ 230419-dailycoding/app/src/main/java/Solution.java
@@ -1,36 +1,36 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        // scanner와 같이 사용자의 입력을 받아오는 클래스이다. 성능차이가 많이 나니 코테에서는
-        BufferedReader bufferedReader =
-            new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        // 문자열을 입력을 받아오는 순서대로 나누는 클래스이다. 토큰라이저에 파라미터로 어떻게 나눌 것인 가를 할 수 있다.
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int count = 1;
+        int startIndex = 1;
+        int endIndex = 1;
+        int sum = 1;
 
-        // suno = 숫자객수 quizNo(질의 갯수)
-        int suNo = Integer.parseInt(stringTokenizer.nextToken());
-        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        // endIndex가 1작을 때 까지 하면은 될 것 같기도 하고
+        while(endIndex != n) {
+            // 합이 자연수 보다 N 보다 작다면은 endIndex를 늘림
+            if(sum < n) {
+                endIndex++;
+                sum += endIndex;
+            }
+            // 합이 자연수 보다 크다면 숫자를 줄여야 하기 떄문에 startIndex += 1
+            else {
+                sum -= startIndex;
+                startIndex++;
+            }
 
-        // 숫자형 데이터는 long이 더 좋다.
-        long[] sum = new long[suNo + 1];
-
-        for(int i = 1; i <= suNo; i +=1) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            sum[i] = sum[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+            if(sum == n) {
+                count++;
+                endIndex++;
+                sum += endIndex;
+            }
         }
 
-        for(int j = 0; j < quizNo; j += 1) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken());
-            int k = Integer.parseInt(stringTokenizer.nextToken());
-
-            System.out.print(sum[k] - sum[i - 1]);
-        }
+        System.out.print(count);
     }
 }
