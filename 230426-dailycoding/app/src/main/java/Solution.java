95c50bb38317e81d88ab7c4f9b11d2b9bf93dc6e
sunghwan22 <tidls3144@naver.com>
2023-04-26 16:53:55 +0900

    백준 절대값 힙 구현하기 강의 시청 후 코드 작성

--- 230426-dailycoding/app/src/main/java/Solution.java
+++ 230426-dailycoding/app/src/main/java/Solution.java
@@ -1,42 +1,49 @@
-import com.google.common.collect.ForwardingQueue;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.PriorityQueue;
 
 public class Solution {
-    // 가장 위에 있는 카드를 바닥에 버린다.
-    // 가장 위에 있는 카드를 가장 아래에 있는 카드 밑으로 옮긴다.
-    // 1
-    // 2     3
-    // 3     4      2
-    // 4     2      4       4
-    // n이 4일경우 이렇게 가장 마지막에 있는 카드는 4이다.
+   // 아하 0이 나오면은 출력을 한다.
+    // 가장 작은수를 출력한다. ( 절대값이 같은 경우 더 작은 값을 출력한다.
+    // 배열이 비어 있으면 0을 출력을 한다.
 
-    // 지금 상황이 데이터의 양쪽에서 삽입과 삭제가 일어나기 때문에 queue를 사용한게 좋을 것 같다.
+    // 데이터가 새로 삽입될 떄마다 절대값과 관련된 정렬이 필요하다.
+    // 그 다음 우선순위 큐를 통해서 쉽게 해결을 할 수 있겠다.
+    // 절대값이 작은 순서대로 정렬을 하고 그대로 출력을 하면은 된다.
+    public static void main(String[] args) throws IOException {
+        // 우선순위 정렬을 커스터마이징 하는 방법을 공부해보자
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            // 절대값이 작은 데이터 우선
+            // 절대값이 같은 경우 음수를 우선해서 정렬한다.
+            int firstAbs = Math.abs(o1);
+            int secondAbs = Math.abs(o2);
 
-        int n = scanner.nextInt();
+            if(firstAbs == secondAbs) {
+                return o1 > o2 ? 1 : -1;
+            }
 
-        Queue<Integer> queue = new LinkedList<>();
+            return firstAbs - secondAbs;
+        });
 
-        for(int i = 1; i <= n; i += 1) {
-            queue.add(i);
+        for(int i = 0; i < n; i +=1) {
+            int request = Integer.parseInt(bufferedReader.readLine());
+            if(request == 0) {
+                if(priorityQueue.size() == 0) {
+                    System.out.println("0");
+                }
+
+                if(priorityQueue.size() != 0) {
+                    System.out.println(priorityQueue.poll());
+                }
+            }
+
+            if(request != 0) {
+                priorityQueue.add(request);
+            }
         }
-
-        while (queue.size() == 1) {
-            int temp = queue.poll();
-
-            queue.add(temp);
-        }
-
-        System.out.print(queue.poll());
     }
 }
