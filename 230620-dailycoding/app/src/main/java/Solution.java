a9f3d5d5873aa17ec74f6eef8094fdf4df7113e1
sunghwan22 <tidls3144@naver.com>
2023-06-20 11:41:06 +0900

    대소문자 바꿔서 출력하기

---
+++ 230620-dailycoding/app/src/main/java/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        String answer = "";
+
+        for(char x : a.toCharArray()) {
+            if(Character.isUpperCase(x)) {
+                answer += Character.toLowerCase(x);
+            }
+
+            if(Character.isLowerCase(x)) {
+                answer += Character.toUpperCase(x);
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
