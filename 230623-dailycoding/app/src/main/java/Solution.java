7a08948148fdb4188b5c00adc1a693b60646c9a8
sunghwan22 <tidls3144@naver.com>
2023-06-25 22:45:01 +0900

    프로그래머스 큰 수 만들기 스택을 이용해서 풀이

--- 230623-dailycoding/app/src/main/java/Solution.java
+++ 230623-dailycoding/app/src/main/java/Solution.java
@@ -4,23 +4,24 @@ class Solution {
     public String solution(String number, int k) {
         // 스택은 후입 선출이다.
         //  큐는 선입 후출
-        char[] result = new char[number.length() - k];
+        char[] result = new char[number.length() -k];
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < number.length(); i += 1) {
+        for(int i =0; i < number.length(); i += 1) {
             char c = number.charAt(i);
 
-            // 스택이 비어있지 않고 && 스택의 가장 위에 있는 요소가 c보다 작고 k가 0보다 클떄 pop을 해서 요소제ㅔ거
-            while (!stack.isEmpty() && stack.peek() < c && k > 0) {
+            while(!stack.isEmpty() && stack.peek() < c && k >0) {
                 stack.pop();
                 k -= 1;
             }
+
             stack.push(c);
         }
 
-        for(int i =0; i < result.length; i += 1) {
+        for(int i = 0; i < result.length; i += 1) {
             result[i] = stack.get(i);
         }
+
         return new String(result);
     }
 }
